9250b9187de9e3fba9a78266997387ebf18729ab
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 01:37:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,11 @@
 words = ['hi', 'hello', 'bye', 'goodbye']
 
 def my_each(array)
- while block_given?
+
    n = 0
    while n < array.length
      yield array[n]
      n += 1
      array
    end
- end 
 end
